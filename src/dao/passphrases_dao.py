57e2827ccdaf31a6cb3088b619213eec39edeb91
Veloxization <github@velo.anonaddy.com>
2022-04-08 23:46:44 +0300

    Added passphrases DAO class

---
+++ src/dao/passphrases_dao.py
@@ -0,0 +1,47 @@
+"""The classes and functions handling data access objects for the passphrases table"""
+from db_connection.db_connector import DBConnection
+
+class PassphrasesDAO:
+    """A data access object for passphrases
+    Attributes:
+        db_address: The address for the database file where the passphrases table resides"""
+
+    def __init__(self, db_address):
+        """Create a new data access object for passphrases
+        Args:
+            db_address: The address for the database file where the passphrases table resides"""
+
+        self.db_connection = DBConnection(db_address)
+
+    def get_all_passphrases(self, server_id: int):
+        """Get all the passphrases a server uses
+        Args:
+            server_id: The ID of the Guild these passphrases belong to
+        Returns: A list of Rows containing the passphrases of the Guild"""
+
+        connection, cursor = self.db_connection.connect_to_db()
+        sql = "SELECT * FROM passphrases WHERE server_id=?"
+        cursor.execute(sql, (server_id,))
+        passphrases = cursor.fetchall()
+        self.db_connection.close_connection(connection)
+        return passphrases
+
+    def add_passphrase(self, server_id: int, content: str):
+        """Add a new verification passphrase to be used on a server
+        Args:
+            server_id: The ID of the Guild the passphrase is associated with
+            content: The passphrase itself"""
+        connection, cursor = self.db_connection.connect_to_db()
+        sql = "INSERT INTO passphrases (server_id, content) VALUES (?, ?)"
+        cursor.execute(sql, (server_id, content))
+        self.db_connection.commit_and_close(connection)
+
+    def delete_passphrase(self, passphrase_id: int):
+        """Delete a passphrase
+        Args:
+            passphrase_id: The database ID of the passphrase to delete"""
+
+        connection, cursor = self.db_connection.connect_to_db()
+        sql = "DELETE FROM passphrases WHERE id=?"
+        cursor.execute(sql, (passphrase_id,))
+        self.db_connection.commit_and_close(connection)
