2e8fdcbae39da5633210d23d6c135e65667f3adb
Veloxization <github@velo.anonaddy.com>
2022-06-07 17:55:17 +0300

    Added guild role service and entity

---
+++ src/entities/guild_role_entity.py
@@ -0,0 +1,25 @@
+"""Guild role database rows converted into Python objects"""
+
+class GuildRoleEntity():
+    """An object derived from the guild roles database table's rows
+    Attributes:
+        db_id: The database ID of the guild role
+        role_id: The Discord ID of the guild role
+        category_id: The database ID of the category this role belongs in
+        guild_id: The Discord ID of the guild this role belongs in
+        category: The name of the category this role belongs in"""
+
+    def __init__(self, db_id: int, role_id: int, category_id: int, guild_id: int, category: str):
+        """Create a new guild role entity
+        Args:
+            db_id: The database ID of the guild role
+            role_id: The Discord ID of the guild role
+            category_id: The database ID of the category this role belongs in
+            guild_id: The Discord ID of the guild this role belongs in
+            category: The name of the category this role belongs in"""
+
+        self.db_id = db_id
+        self.role_id = role_id
+        self.category_id = category_id
+        self.guild_id = guild_id
+        self.category = category
---
+++ src/services/guild_role_service.py
@@ -0,0 +1,81 @@
+"""The guild role service is used to call methods in the guild roles DAO class."""
+
+from dao.guild_roles_dao import GuildRolesDAO
+from entities.guild_role_entity import GuildRoleEntity
+
+class GuildRoleSerivce:
+    """A service for calling methods from guild roles DAO
+    Attributes:
+        guild_role_dao: The DAO object this service will use"""
+
+    def __init__(self, db_address):
+        """Create a new service for guild roles DAO
+        Args:
+            db_address: The address for the database file where the guild roles table resides"""
+
+        self.guild_roles_dao = GuildRolesDAO(db_address)
+
+    def _convert_to_entity(self, row):
+        """Convert a database row to a guild role entity
+        Args:
+            row: The database row to convert to a guild row entity
+        Returns: A guild role entity equivalent to the database row"""
+
+        return GuildRoleEntity(row["id"], row["role_id"], row["category_id"], row["guild_id"],
+                               row["category"])
+
+    def get_all_guild_roles(self, guild_id: int):
+        """Get all guild roles of a specified Guild
+        Args:
+            guild_id: The ID of the Discord Guild to look for roles
+        Returns: A list of guild role entities"""
+
+        rows = self.guild_roles_dao.get_all_guild_roles(guild_id)
+        return [self._convert_to_entity(row) for row in rows]
+
+    def get_guild_roles_of_type(self, role_category: str, guild_id: int):
+        """Get all guild roles of specific type
+        Args:
+            role_category: The category of role to get (e.g. ADMIN, NEW, VERIFIED etc.)
+            guild_id: The ID of the Discord Guild to look for roles
+        Returns: A list of guild role entities"""
+
+        rows = self.guild_roles_dao.get_guild_roles_of_type(role_category, guild_id)
+        return [self._convert_to_entity(row) for row in rows]
+
+    def get_guild_roles_by_role_id(self, role_id: int):
+        """Get guild roles with a specific ID
+        Args:
+            role_id: The ID of the role from the Discord Guild
+        Returns: A list of guild role entities"""
+
+        rows = self.guild_roles_dao.get_guild_roles_by_role_id(role_id)
+        return [self._convert_to_entity(row) for row in rows]
+
+    def add_guild_role(self, role_id: int, category_id: int):
+        """Add a role under a guild role category
+        Args:
+            role_id: The ID of the role from the Discord Guild
+            category_id: The database ID of the category this role belongs in"""
+
+        self.guild_roles_dao.add_guild_role(role_id, category_id)
+
+    def remove_guild_role_from_category(self, role_id: int, category_id: int):
+        """Remove a guild role by the role's Discord ID
+        Args:
+            role_id: The Discord ID of the role to remove
+            category_id: The database ID of the category to remove this role from"""
+
+        self.guild_roles_dao.remove_guild_role_from_category(role_id, category_id)
+
+    def delete_guild_roles(self, guild_id: int):
+        """Delete all guild roles of a given guild
+        Args:
+            guild_id: The Discord ID of the guild whose guild roles to delete"""
+
+        self.guild_roles_dao.delete_guild_roles(guild_id)
+
+    def clear_guild_roles_table(self):
+        """Delete every single guild role"""
+
+        self.guild_roles_dao.clear_guild_roles_table()
