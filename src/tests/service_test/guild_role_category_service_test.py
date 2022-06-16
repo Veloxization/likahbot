f8143decf3d99e7954b7d74f9a83ca4bbffafb03
Veloxization <github@velo.anonaddy.com>
2022-06-16 18:58:56 +0300

    Guild role category service tests

---
+++ src/tests/service_test/guild_role_category_service_test.py
@@ -0,0 +1,38 @@
+import unittest
+import os
+from services.guild_role_category_service import GuildRoleCategoryService
+
+class TestGuildRoleCategoryService(unittest.TestCase):
+    def setUp(self):
+        db_address = "database/test_db.db"
+        os.popen(f"sqlite3 {db_address} < database/schema.sql")
+        self.guild_role_category_service = GuildRoleCategoryService(db_address)
+
+    def tearDown(self):
+        self.guild_role_category_service.clear_guild_role_categories()
+
+    def test_guild_role_categories_are_found_correctly(self):
+        self.guild_role_category_service.add_guild_role_category(1234, "TEST")
+        categories = self.guild_role_category_service.get_all_guild_role_categories(1234)
+        self.assertEqual(categories[0].category, "TEST")
+
+    def test_guild_role_categories_are_removed_correctly(self):
+        self.guild_role_category_service.add_guild_role_category(1234, "TEST")
+        categories = self.guild_role_category_service.get_all_guild_role_categories(1234)
+        self.assertEqual(len(categories), 1)
+        self.guild_role_category_service.remove_guild_role_category(categories[0].db_id)
+        categories = self.guild_role_category_service.get_all_guild_role_categories(1234)
+        self.assertEqual(len(categories), 0)
+
+    def test_all_guild_role_categories_are_removed_correctly(self):
+        self.guild_role_category_service.add_guild_role_category(1234, "TEST")
+        self.guild_role_category_service.add_guild_role_category(2345, "TEST")
+        categories1 = self.guild_role_category_service.get_all_guild_role_categories(1234)
+        categories2 = self.guild_role_category_service.get_all_guild_role_categories(2345)
+        self.assertEqual(len(categories1), 1)
+        self.assertEqual(len(categories2), 1)
+        self.guild_role_category_service.remove_all_guild_role_categories(1234)
+        categories1 = self.guild_role_category_service.get_all_guild_role_categories(1234)
+        categories2 = self.guild_role_category_service.get_all_guild_role_categories(2345)
+        self.assertEqual(len(categories1), 0)
+        self.assertEqual(len(categories2), 1)
