e8be38e3cca1dbb3aa3c61c317a360b56150e3ea
Veloxization <github@velo.anonaddy.com>
2022-08-17 15:48:21 +0300

    Added temp ban service tests

---
+++ src/tests/service_test/temp_ban_service_test.py
@@ -0,0 +1,70 @@
+import unittest
+import os
+from datetime import datetime, timedelta
+from services.temp_ban_service import TempBanService
+
+class TestTempBanService(unittest.TestCase):
+    def setUp(self):
+        db_address = "database/test_db.db"
+        os.popen(f"sqlite3 {db_address} < database/schema.sql")
+        self.temp_ban_service = TempBanService(db_address)
+        self.expiration1 = datetime.utcnow() + timedelta(days=1)
+        self.expiration2 = datetime.utcnow() + timedelta(days=-1)
+
+    def tearDown(self):
+        self.temp_ban_service.clear_temp_bans()
+
+    def test_guild_temp_bans_are_found_correctly(self):
+        temp_bans = self.temp_ban_service.get_guild_temp_bans(9876)
+        self.assertEqual(len(temp_bans), 0)
+        self.temp_ban_service.create_temp_ban(1234, 9876, self.expiration1)
+        temp_bans = self.temp_ban_service.get_guild_temp_bans(9876)
+        self.assertEqual(len(temp_bans), 1)
+
+    def test_expired_temp_bans_are_found_correctly(self):
+        self.temp_ban_service.create_temp_ban(1234, 9876, self.expiration2)
+        self.temp_ban_service.create_temp_ban(2345, 9876, self.expiration1)
+        self.temp_ban_service.create_temp_ban(1234, 8765, self.expiration2)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 2)
+        self.assertEqual(temp_bans[0].user_id, temp_bans[1].user_id)
+
+    def test_temp_bans_are_edited_correctly(self):
+        self.temp_ban_service.create_temp_ban(1234, 9876, self.expiration1)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 0)
+        self.temp_ban_service.edit_temp_ban(1234, 9876, self.expiration2)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 1)
+
+    def test_temp_bans_are_deleted_correctly(self):
+        self.temp_ban_service.create_temp_ban(1234, 9876, self.expiration2)
+        self.temp_ban_service.create_temp_ban(1234, 8765, self.expiration2)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 2)
+        self.temp_ban_service.delete_temp_ban(1234, 9876)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 1)
+        self.assertEqual(temp_bans[0].guild_id, 8765)
+
+    def test_user_temp_bans_are_deleted_correctly(self):
+        self.temp_ban_service.create_temp_ban(1234, 9876, self.expiration2)
+        self.temp_ban_service.create_temp_ban(2345, 9876, self.expiration2)
+        self.temp_ban_service.create_temp_ban(1234, 8765, self.expiration2)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 3)
+        self.temp_ban_service.delete_user_temp_bans(1234)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 1)
+        self.assertNotEqual(temp_bans[0].user_id, 1234)
+
+    def test_guild_temp_bans_are_deleted_correctly(self):
+        self.temp_ban_service.create_temp_ban(1234, 9876, self.expiration2)
+        self.temp_ban_service.create_temp_ban(2345, 9876, self.expiration2)
+        self.temp_ban_service.create_temp_ban(1234, 8765, self.expiration2)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 3)
+        self.temp_ban_service.delete_guild_temp_bans(9876)
+        temp_bans = self.temp_ban_service.get_expired_temp_bans()
+        self.assertEqual(len(temp_bans), 1)
+        self.assertNotEqual(temp_bans[0].guild_id, 9876)
