fbeae93482973ed2eb9f389bb925a87702814604
Veloxization <github@velo.anonaddy.com>
2023-04-13 22:00:26 +0300

    Added time zone service tests

---
+++ src/tests/service_test/time_zone_service_test.py
@@ -0,0 +1,50 @@
+import unittest
+import os
+from services.time_zone_service import TimeZoneService
+
+class TestTimeZoneService(unittest.TestCase):
+    def setUp(self):
+        db_address = "database/test_db.db"
+        os.popen(f"sqlite3 {db_address} < database/schema.sql")
+        self.time_zone_service = TimeZoneService(db_address)
+
+    def tearDown(self):
+        self.time_zone_service.clear_time_zones()
+
+    def test_user_time_zone_is_found_correctly(self):
+        self.time_zone_service.add_user_time_zone(1234, "US/Eastern")
+        tz = self.time_zone_service.get_user_time_zone(1234)
+        self.assertEqual(tz.time_zone, "US/Eastern")
+
+    def test_user_time_zone_defaults_to_UTC_correctly(self):
+        tz = self.time_zone_service.get_user_time_zone(1234)
+        self.assertEqual(tz.time_zone, "UTC")
+
+    def test_time_zone_is_found_correctly_by_id(self):
+        tz_id = self.time_zone_service.add_user_time_zone(1234, "US/Eastern")
+        tz = self.time_zone_service.get_time_zone_by_id(tz_id)
+        self.assertEqual(tz.time_zone, "US/Eastern")
+
+    def test_user_time_zone_is_edited_correctly(self):
+        self.time_zone_service.add_user_time_zone(1234)
+        self.time_zone_service.edit_user_time_zone(1234, "US/Eastern")
+        tz = self.time_zone_service.get_user_time_zone(1234)
+        self.assertEqual(tz.time_zone, "US/Eastern")
+
+    def test_time_zone_is_edited_correctly_by_id(self):
+        tz_id = self.time_zone_service.add_user_time_zone(1234)
+        self.time_zone_service.edit_time_zone_by_id(tz_id, "US/Eastern")
+        tz = self.time_zone_service.get_time_zone_by_id(tz_id)
+        self.assertEqual(tz.time_zone, "US/Eastern")
+
+    def test_user_time_zone_is_deleted_correctly(self):
+        self.time_zone_service.add_user_time_zone(1234, "US/Eastern")
+        self.time_zone_service.delete_user_time_zone(1234)
+        tz = self.time_zone_service.get_user_time_zone(1234)
+        self.assertEqual(tz.time_zone, "UTC")
+
+    def test_time_zone_is_deleted_correctly_by_id(self):
+        tz_id = self.time_zone_service.add_user_time_zone(1234, "US/Eastern")
+        self.time_zone_service.delete_time_zone_by_id(tz_id)
+        tz = self.time_zone_service.get_time_zone_by_id(tz_id)
+        self.assertIsNone(tz)
