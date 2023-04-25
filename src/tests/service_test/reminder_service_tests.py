93829a68c1f0005e7f08bb9667a9282001de7247
Veloxization <github@velo.anonaddy.com>
2023-04-25 23:38:06 +0300

    Added reminder service tests

---
+++ src/tests/service_test/reminder_service_tests.py
@@ -0,0 +1,143 @@
+import unittest
+import os
+from datetime import datetime, timedelta
+from services.reminder_service import ReminderService
+
+class TestReminderService(unittest.TestCase):
+    def setUp(self):
+        db_address = "database/test_db.db"
+        os.popen(f"sqlite3 {db_address} < database/schema.sql")
+        self.reminder_service = ReminderService(db_address)
+        self.date1 = datetime.utcnow() + timedelta(days=1)
+        self.date2 = datetime.utcnow() + timedelta(days=2)
+        self.date3 = datetime.utcnow() + timedelta(days=3)
+        self.expired1 = datetime.utcnow() + timedelta(days=-1)
+        self.expired2 = datetime.utcnow() + timedelta(days=-2)
+
+    def tearDown(self):
+        self.reminder_service.clear_reminders()
+
+    def test_reminders_by_user_are_found_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(2345, 8765, "test2", self.date2, "time")
+        self.reminder_service.add_new_reminder(1234, 8765, "test3", self.date3, "time")
+        reminders = self.reminder_service.get_reminders_by_user(1234)
+        self.assertEqual(len(reminders), 2)
+        self.assertEqual(reminders[0].content, "test1")
+        self.assertEqual(reminders[1].content, "test3")
+
+    def test_public_reminders_by_user_are_found_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(1234, 9876, "test2", self.date1, "time", is_public=True)
+        reminders = self.reminder_service.get_public_reminders_by_user(1234)
+        self.assertEqual(len(reminders), 1)
+        self.assertEqual(reminders[0].content, "test2")
+
+    def test_reminders_by_user_in_guild_are_found_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(1234, 8765, "test2", self.date1, "time")
+        self.reminder_service.add_new_reminder(2345, 8765, "test3", self.date1, "time")
+        reminders = self.reminder_service.get_reminders_by_user_in_guild(1234, 8765)
+        self.assertEqual(len(reminders), 1)
+        self.assertEqual(reminders[0].content, "test2")
+
+    def test_public_reminders_by_user_in_guild_are_found_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(1234, 9876, "test2", self.date2, "time", is_public=True)
+        self.reminder_service.add_new_reminder(1234, 8765, "test3", self.date3, "time", is_public=True)
+        reminders = self.reminder_service.get_public_reminders_by_user_in_guild(1234, 9876)
+        self.assertEqual(len(reminders), 1)
+        self.assertEqual(reminders[0].content, "test2")
+
+    def test_reminders_in_guild_are_found_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(2345, 9876, "test2", self.date2, "time")
+        self.reminder_service.add_new_reminder(3456, 8765, "test3", self.date3, "time")
+        reminders = self.reminder_service.get_reminders_in_guild(9876)
+        self.assertEqual(len(reminders), 2)
+        self.assertEqual(reminders[0].content, "test1")
+        self.assertEqual(reminders[1].content, "test2")
+
+    def test_public_reminders_in_guild_are_found_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(2345, 9876, "test2", self.date2, "time", is_public=True)
+        self.reminder_service.add_new_reminder(3456, 8765, "test3", self.date3, "time", is_public=True)
+        reminders = self.reminder_service.get_public_reminders_in_guild(9876)
+        self.assertEqual(len(reminders), 1)
+        self.assertEqual(reminders[0].content, "test2")
+
+    def test_expired_reminders_are_found_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(2345, 9876, "test2", self.expired1, "time")
+        self.reminder_service.add_new_reminder(3456, 8765, "test3", self.expired2, "time")
+        reminders = self.reminder_service.get_expired_reminders()
+        self.assertEqual(len(reminders), 2)
+        self.assertEqual(reminders[0].content, "test3")
+        self.assertEqual(reminders[1].content, "test2")
+
+    def test_reminder_is_found_correctly_by_id(self):
+        reminder_id = self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        reminder = self.reminder_service.get_reminder_by_id(reminder_id)
+        self.assertIsNotNone(reminder)
+
+    def test_reminder_is_edited_correctly(self):
+        reminder_id = self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.edit_reminder(reminder_id, "test2", self.date2, True, 60, "date", 3)
+        reminder = self.reminder_service.get_reminder_by_id(reminder_id)
+        self.assertEqual(reminder.content, "test2")
+        self.assertEqual(reminder.reminder_date, datetime.strftime(self.date2, "%Y-%m-%d %H:%M:%S.%f"))
+        self.assertTrue(reminder.is_public)
+        self.assertEqual(reminder.interval, 60)
+        self.assertEqual(reminder.reminder_type, "date")
+        self.assertEqual(reminder.repeats_left, 3)
+
+    def test_reminder_repeats_are_updated_correctly(self):
+        reminder_id = self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time", repeats=3)
+        self.reminder_service.update_reminder_repeats(reminder_id)
+        reminder = self.reminder_service.get_reminder_by_id(reminder_id)
+        self.assertEqual(reminder.repeats_left, 2)
+
+    def test_user_reminders_are_deleted_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(2345, 9876, "test2", self.date2, "time")
+        self.reminder_service.add_new_reminder(1234, 8765, "test3", self.date3, "time")
+        self.reminder_service.delete_user_reminders(1234)
+        reminders = self.reminder_service.get_reminders_by_user(1234)
+        self.assertEqual(len(reminders), 0)
+        reminders = self.reminder_service.get_reminders_by_user(2345)
+        self.assertEqual(len(reminders), 1)
+
+    def test_user_reminders_in_guild_are_deleted_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(1234, 8765, "test2", self.date2, "time")
+        self.reminder_service.delete_user_reminders_in_guild(1234, 9876)
+        reminders = self.reminder_service.get_reminders_by_user(1234)
+        self.assertEqual(len(reminders), 1)
+        self.assertEqual(reminders[0].content, "test2")
+
+    def test_reminders_in_guild_are_deleted_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.add_new_reminder(1234, 9876, "test2", self.date2, "time")
+        self.reminder_service.add_new_reminder(1234, 8765, "test3", self.date3, "time")
+        self.reminder_service.delete_guild_reminders(9876)
+        reminders = self.reminder_service.get_reminders_by_user(1234)
+        self.assertEqual(len(reminders), 1)
+        self.assertEqual(reminders[0].content, "test3")
+
+    def test_reminders_are_deleted_correctly_by_id(self):
+        reminder_id = self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time")
+        self.reminder_service.delete_reminder_by_id(reminder_id)
+        reminder = self.reminder_service.get_reminder_by_id(reminder_id)
+        self.assertIsNone(reminder)
+
+    def test_reminders_with_no_repeats_are_deleted_correctly(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time", repeats=0)
+        self.reminder_service.delete_reminders_with_no_repeats()
+        reminders = self.reminder_service.get_reminders_by_user(1234)
+        self.assertEqual(len(reminders), 0)
+
+    def test_reminders_with_negative_repeats_are_not_inadvertently_deleted(self):
+        self.reminder_service.add_new_reminder(1234, 9876, "test1", self.date1, "time", repeats=-1)
+        self.reminder_service.delete_reminders_with_no_repeats()
+        reminders = self.reminder_service.get_reminders_by_user(1234)
+        self.assertEqual(len(reminders), 1)
